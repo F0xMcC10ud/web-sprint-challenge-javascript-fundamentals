2c1c758f69d2df07c40e028eb181991a4615aae5
Irving Delgado <rvngdelgado@gmail.com>
2022-01-20 21:33:22 -0600

    fixed an error in coding, CuboidMaker is now returning the correct values

--- index.js
+++ index.js
@@ -169,7 +169,7 @@ function CuboidMaker({length, width, height}){
   Create a method called volume using CuboidMaker's prototype that returns the volume of a given cuboid's length, width, and height
   Formula for cuboid volume: length * width * height   */
 CuboidMaker.prototype.volume = function(){
-  return this.length * this.width * this.height; 
+  return this.length * this.width * this.height
 }
 
 
@@ -180,7 +180,7 @@ CuboidMaker.prototype.volume = function(){
   Formula for cuboid surface area of a cube: 
   2 * (length * width + length * height + width * height)  */
 CuboidMaker.prototype.surfaceArea = function(){
-  return 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
+  return 2 * (this.length * this.width + this.length * this.height + this.width * this.height)
 }
 
 
@@ -189,7 +189,11 @@ CuboidMaker.prototype.surfaceArea = function(){
 /* 🐴🐴🐴 Step 4: Create a new object that uses CuboidMaker 🐴🐴🐴
   Create an object called cuboid that uses the new keyword to use our CuboidMaker constructor
   Add properties and values of length: 4, width: 5, and height: 5 to cuboid. */
-const cuboid = new CuboidMaker(4, 5, 5);
+const cuboid = new CuboidMaker({
+  length: 4,
+  width: 5,
+  height: 5
+});
 
 
 
